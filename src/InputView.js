c097d3a488177fbc1b6d07ff951d2c5d15f0e2d2
yoovin <soo22839@gmail.com>
2023-11-15 01:00:14 +0900

    fix: 내려쓰기 추가

--- src/InputView.js
+++ src/InputView.js
@@ -23,7 +23,7 @@ const DRINKS = ['제로콜라', '레드와인', '샴페인'];
 const InputView = {
     async readDate() {
         const input = await Console.readLineAsync(
-            '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
+            '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n'
         );
         if (this.dateValidate(input)) {
             return Number(input);
@@ -48,7 +48,7 @@ const InputView = {
 
     async readMenu() {
         const input = await Console.readLineAsync(
-            '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)'
+            '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n'
         );
         if (this.menuValidate(input)) {
             return this.inputMenus(input);
