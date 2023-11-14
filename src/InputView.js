e192de243df030d074f3a32974df2ec5ef71eac5
yoovin <soo22839@gmail.com>
2023-11-14 23:32:37 +0900

    feat: date 입력 구현

--- src/InputView.js
+++ src/InputView.js
@@ -3,9 +3,26 @@ const InputView = {
         const input = await Console.readLineAsync(
             '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
         );
-        // ...
+        if (this.dateValidate(input)) {
+            return Number(input);
+        }
     },
-    // ...
+
+    dateValidate(date) {
+        if (!/^\d+$/.test(date)) {
+            throw new Error(
+                '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.'
+            );
+        }
+
+        if (Number(date) < 1 || Number(date) > 31) {
+            throw new Error(
+                '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.'
+            );
+        }
+
+        return true;
+    },
 };
 
 export default InputView;
