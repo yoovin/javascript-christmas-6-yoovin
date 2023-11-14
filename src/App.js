9db508ba0ffc765d4aa070fe969c9b1b40e1f683
yoovin <soo22839@gmail.com>
2023-11-15 00:29:52 +0900

    feat: 총 금액 계산 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MENU_PRICES = {
+    양송이수프: 6000,
+    타파스: 5500,
+    시저샐러드: 8000,
+    티본스테이크: 55000,
+    바비큐립: 54000,
+    해산물파스타: 35000,
+    크리스마스파스타: 25000,
+    초코케이크: 15000,
+    아이스크림: 5000,
+    제로콜라: 3000,
+    레드와인: 60000,
+    샴페인: 25000,
+};
+
 class App {
     async run() {}
+
+    calculateTotalPrice(menus) {
+        let totalPrice = 0;
+        for (const menu in menus) {
+            totalPrice += MENU_PRICES[menu] * menus[menu];
+        }
+        return totalPrice;
+    }
 }
 
 export default App;
